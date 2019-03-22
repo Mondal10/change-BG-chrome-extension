7536c10761bc880fd1b7f1bf9da2679c3cb20270
Mondal10 <amitmondal111.am@gmail.com>
2019-03-22 19:45:23 +0530

    button color tweeks and code cleaning

--- popup.js
+++ popup.js
@@ -1,10 +1,9 @@
 let buttons = document.getElementsByTagName("button");
 
 for(let button of buttons){
-  // console.log(button);
   button.addEventListener('click',(element)=>{
-    console.log("helooo",element.target.id);
-    let color = element.target.id;
+    // console.log("helooo",element.target.style.backgroundColor);
+    let color = element.target.style.backgroundColor;
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
       chrome.tabs.executeScript(tabs[0].id, {
         code: `document.body.style.backgroundColor = '${color}';`
