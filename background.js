20f9b647f71f5fa3a0052d63858d545e2c4f188d
Mondal10 <amitmondal111.am@gmail.com>
2019-03-24 15:31:12 +0530

    not useful at moment

--- background.js
+++ background.js
@@ -1,18 +1,20 @@
-chrome.runtime.onInstalled.addListener(function () {
-  // chrome.storage.sync.set({ color: "salmon" }, function() {
-  //   console.log("The color is SALMON");
-  // });
+// chrome.runtime.onInstalled.addListener(function () {
+//   // chrome.storage.sync.set({ color: "salmon" }, function() {
+//   //   console.log("The color is SALMON");
+//   // });
 
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-    chrome.declarativeContent.onPageChanged.addRules([
-      {
-        conditions: [
-          new chrome.declarativeContent.PageStateMatcher({
-            // pageUrl: { hostEquals: "developer.chrome.com" }
-          })
-        ],
-        actions: [new chrome.declarativeContent.ShowPageAction()]
-      }
-    ]);
-  });
-});
+//   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+//     chrome.declarativeContent.onPageChanged.addRules([
+//       {
+//         conditions: [
+//           new chrome.declarativeContent.PageStateMatcher({
+//             // pageUrl: { hostEquals: "developer.chrome.com" }
+//           })
+//         ],
+//         actions: [new chrome.declarativeContent.ShowPageAction()]
+//       }
+//     ]);
+//   });
+// });
+
+// If you want to use want to use chrome extension for specifi url use above
