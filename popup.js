a4dce08ae61eb20a114179b1c4af24ff53103d54
Mondal10 <amitmondal111.am@gmail.com>
2019-03-24 14:02:12 +0530

    added change text color button

--- popup.js
+++ popup.js
@@ -1,22 +1,38 @@
-let backgroundButtons = document.getElementsByClassName("backgroundButton");
-let textButtons = document.getElementsByClassName("textButton");
+const backgroundButtons = document.getElementsByClassName("backgroundButton");
+const textButtons = document.getElementsByClassName("textButton");
+
+
+const bgbuttonArr = ['salmon', 'antiquewhite', 'lightslategray', 'black', 'cornflowerblue', 'darkolivegreen', '#222830', '#002b36'];
+const textbuttonArr = ['white', 'antiquewhite', 'lightslategray', '#1a1d24'];
+
+bgbuttonArr.forEach((color) => {
+
+});
 
 for (let backgroundButton of backgroundButtons) {
   backgroundButton.addEventListener('click', (element) => {
-    // console.log("helooo",element.target.style.backgroundColor);
     let bgColor = element.target.style.backgroundColor;
     /***Used to run a one line query on webpage***/
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      // chrome.tabs.executeScript(tabs[0].id, {
-      //   code: `document.body.style.backgroundColor = '${color}';${console.log("ek:::::")}; 
-      //   for(var i =0;i<4;i++){console.log(i)}`
-      // });
       chrome.tabs.executeScript(tabs[0].id, {
         code: `document.body.style.backgroundColor = '${bgColor}';
                 var divs = document.getElementsByTagName('div'); 
                 for (let div of divs) {
                   div.style.backgroundColor = 'transparent';
                 }`
+      });
+    });
+    /***Used to run a one line query on webpage***/
+  });
+}
+
+for (let textButton of textButtons) {
+  textButton.addEventListener('click', (element) => {
+    let textColor = element.target.style.backgroundColor;
+    /***Used to run a one line query on webpage***/
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.executeScript(tabs[0].id, {
+        code: `document.body.style.color = '${textColor}'`
       });
     });
     /***Used to run a one line query on webpage***/
